0a65646c4c5ee2162b9d520be120efe55765c5c7
dongsu-iis <dongsu.workout@outlook.jp>
2018-08-08 18:18:14 +0900

    コメントを修正

--- FizzBuzz/main.js
+++ FizzBuzz/main.js
@@ -8,7 +8,7 @@ JavaScript
 function printFizzBuzz(start = 1, end = 100) {
     if (start <= end) {
         // 効率を考え、剰余の計算回数をなるべく減らす
-        // 0が判定がFizzBuzzにならないように、条件式に入れる
+        // 0の判定がFizzBuzzにならないように、条件式に入れる
         if (start !== 0 && start % 3 === 0) {
 
             if (start % 5 === 0) {
